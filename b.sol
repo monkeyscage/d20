dd188f16e4b8fbf9d502d61ebf05b35fa87f5fde
maxxflyer <maxxflyer@gmail.com>
2018-01-29 05:35:38 +0100

    Update b.sol

--- b.sol
+++ b.sol
@@ -1,23 +1,22 @@
 contract D20Place is SuperOwned {
 string public name;
-string public link;
 address public location;
 
 uint[] public sets;
 address[] public slots;
+mapping(address => bool)public masters;
 
-address public master;
 address public brain;
 address public body;
 
     modifier onlyMaster { 
-        if (msg.sender != master) revert();
+        if (!masters[msg.sender]) revert();
         _;
     }
 
 function D20Place(string _name,address _master,uint _sex,uint _class,uint _alignment,uint _race){
 name=_name;
-master=_master;
+masters[_master]=true;
 sets.push(_race);
 sets.push(_sex);
 sets.push(_class);
@@ -60,40 +59,37 @@ sets[sets.length++]=char;//charisma
 sets[5]=cons; //healt
 }
 
-function setPrice(uint _price)onlyOwner{
+function setPrice(uint _price)public onlyOwner{
 sets[15]=_price;
 }
 
-function setHoldPrice(uint _price)onlyOwner{
+function setHoldPrice(uint _price)public onlyOwner{
 sets[16]=_price;
 }
 
-function setLink(string _link)onlyOwner{
-link=_link; 
-}
-
-function set(uint _i,uint _j)onlyMaster{
+function set(uint _i,uint _j)public onlyMaster{
 sets[_i]=_j;
-}
+} 
 
-function  wear(uint _i,address _j)onlyMaster{
+function  wear(uint _i,address _j)public onlyMaster{
+if(location==address("0x0"))revert();
 slots[_i]=_j;
 }
 
-function setLocation(address _location)onlyMaster{
+function setLocation(address _location)public onlyMaster{
 location=_location;
 }
 
-function setMaster(address _master)onlyMaster{
-master=_master;
+function setMaster(address _master,bool b)public onlyMaster{
+masters[_master]=b;
 }
 
 
-function setBrain(address a)onlyOwner{
+function setBrain(address a)public onlyOwner{
 brain=a;
 } 
 
-function setBody(address a)onlyOwner{
+function setBody(address a)public onlyOwner{
 body=a;
 }
 
