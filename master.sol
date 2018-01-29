d40a8bb3ecfbc3b572f1581babc4a3c688020b98
maxxflyer <maxxflyer@gmail.com>
2018-01-29 04:50:04 +0100

    Update master.sol

--- master.sol
+++ master.sol
@@ -9,4 +9,16 @@ function setOwner(address a)onlyOwner{
 owner=a;
 }
 
+function setPlayer(address _player,uint _set,uint _value)onlyPermissioned{
+
+}
+
+function setPlayerSlot(address _player,uint _slot,address _item)onlyPermissioned{
+
+}
+
+function setPlayerOwner(address _player,address _owner)onlyPermissioned{
+
+}
+
 }
